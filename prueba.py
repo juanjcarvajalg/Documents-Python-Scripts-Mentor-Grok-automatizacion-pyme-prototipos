45c3f9896ee540afe8bacab62589f612df09886f
juanjcarvajalg <juanjcarvajalg@gmail.com>
2026-03-15 12:16:25 -0400

    Update prueba.py
    Prueba cambio local después de pull

--- prueba.py
+++ prueba.py
@@ -1,3 +1,4 @@
 print("¡Hola, soy Juan y estoy empezando mi camino a la agencia de automatización!")
-nombre = "Juan J. Carvajal G."
+nombre = "Juan J. Carvajal Galvis"
 print(f"Bienvenido {nombre} al portafolio de prototipos 🚀")
+print("Prueba después de sincronizar - Juan J. 2026")
