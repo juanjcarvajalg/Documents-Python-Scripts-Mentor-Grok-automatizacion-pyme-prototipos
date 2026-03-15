80b7e96def066983fabccf43c21d7f34979ab408
juanjcarvajalg <juanjcarvajalg@gmail.com>
2026-03-15 11:40:14 -0400

    Add files via upload

--- prueba.py
+++ prueba.py
@@ -1,3 +1,3 @@
 print("¡Hola, soy Juan y estoy empezando mi camino a la agencia de automatización!")
-nombre = "Juan J."
+nombre = "Juan J. Carvajal G."
 print(f"Bienvenido {nombre} al portafolio de prototipos 🚀")
